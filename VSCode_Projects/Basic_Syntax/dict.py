bbdb13d7e2665794e2419d32583a40dcbd6dca23
Malak <malabo01@iba.dk>
2025-12-16 18:22:24 +0100

    Update dict.py

--- VSCode_Projects/Basic_Syntax/dict.py
+++ VSCode_Projects/Basic_Syntax/dict.py
@@ -3,11 +3,8 @@ person = {
     "navn":"Malak",
     "alder": 20,
     "by": "Kolding",
-    "skole": "IBA",
-    "major": "IT-AKITEKTUR"
 }
 print(person["navn"])
 print(person["alder"])
 print(person["by"])
-print(person["skole"])
-print(person["major"])
+
